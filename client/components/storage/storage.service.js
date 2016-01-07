e64628fcc0d45877140f5420db324079fe4a0d60
dan <marvel94@nhnnext.org>
2016-01-08 03:01:40 +0900

    init

---
+++ client/components/storage/storage.service.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+    angular
+        .module('angularFullStackApp')
+        .service('storageService', storageService);
+
+    /* @ngInject */
+    function storageService() {
+        this.setValue = function (key,value,options) {
+            angular.element.jStorage.set(key,value,options);
+        };
+
+        this.getValue = function (key) {
+            return angular.element.jStorage.get(key);
+        };
+
+        this.removeValue = function (key) {
+            angular.element.jStorage.deleteKey(key);
+        };
+
+        //ttl is milliseconds
+        this.setTTL = function (key, ttl) {
+            angular.element.jStorage.setTTL(key,ttl);
+        }
+    }
+})();
+
