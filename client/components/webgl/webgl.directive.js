0e3ffd0622735bd70b9803aca83e392f485cffd9
dan <marvel94@nhnnext.org>
2016-01-11 19:49:01 +0900

    init webgl

--- client/components/webgl/webgl.directive.js
+++ client/components/webgl/webgl.directive.js
@@ -3,6 +3,7 @@
 angular.module('angularFullstackApp')
   .directive('webgl', function () {
     return {
+      controller: 'WebglCtrl',
       templateUrl: 'components/webgl/webgl.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
